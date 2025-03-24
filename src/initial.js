ffee2f2c3917854550073285dd55908df5a66a99
Just-AgA <161239277+Just-AgA@users.noreply.github.com>
2025-03-24 20:55:57 +0100

    Add paragraph component of the initial page

--- src/initial.js
+++ src/initial.js
@@ -4,4 +4,10 @@ const pageComponent = () => {
     return page;
 }
 
+const paragraphComponent = () => {
+    const paragraph = document.createElement("p");
+    paragraph.innerHTML = "Welcome to <strong>Brew & Bites</strong> restaurant,a charming spot where every sip of coffee and bite of food feels like home. Whether you're here to relax with a warm latte or enjoy a delicious meal, our cozy ambiance and friendly service will make you feel right at ease. From savory sides to hearty main dishes, we’ve got something to satisfy every craving."
+    return paragraph;
+}
+
 
