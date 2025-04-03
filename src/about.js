6e09a41ad6c569f8d449b6b1acf431a2e131f614
Just-AgA <161239277+Just-AgA@users.noreply.github.com>
2025-04-03 21:36:58 +0200

    Add Contact component

--- src/about.js
+++ src/about.js
@@ -13,3 +13,26 @@ function aboutDiv() {
     return aboutPage;
 }
 
+function contactDiv() {
+    const contact = document.createElement("div");
+    contact.classList.add("contact");
+    const contactHeading = document.createElement("h2");
+    contactHeading.innerHTML = "Contact Us";
+    contact.appendChild(contactHeading);
+
+    const contactPara = document.createElement("p");
+    contactPara.innerHTML = "<strong>Address:</strong> </br>1234 Craft Ave,Flavor Town, FT 56789";
+
+    contact.appendChild(contactPara);
+
+    const contactTelephone = document.createElement("p");
+    contactTelephone.innerHTML = "<strong>Telephone:</strong></br> +1 (555) 123-4567";
+    contact.appendChild(contactTelephone);
+
+    const contactEmail = document.createElement("p");
+    contactEmail.innerHTML = "<strong>Email:</strong> </br>info@brewbites.com";
+    contact.appendChild(contactEmail);
+    
+    return contact;
+}
+
